454a8dd2092441a99e5ee863801e8de3e869f7ca
kaps2002 <karpandey2002@gmail.com>
2024-07-27 12:54:13 +0530

    added funtionality in the logout section

---
+++ Pustakalya/MainViews/HomeViews/ProfileUI/EditProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  EditProfileView.swift
+//  Pustakalya
+//
+//  Created by Roro on 14/07/24.
+//
+
+import SwiftUI
+
+struct EditProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditProfileView()
+}
